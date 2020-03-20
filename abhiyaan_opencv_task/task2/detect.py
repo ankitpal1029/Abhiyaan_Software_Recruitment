342cfeee94cd46999d8ee6e53671ec51e2c54de5
ankitpal1029 <49815751+ankitpal1029@users.noreply.github.com>
2020-03-20 22:41:37 +0530

    Add files via upload

---
+++ abhiyaan_opencv_task/task2/detect.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+for i in range(1,4):
+    img=cv2.imread(str(i)+".png")
+    a=np.asarray(img)
+    if a[int(a.shape[0]/3)-int(a.shape[0]/6)][int(a.shape[1]/2)][2] == 255:
+        cv2.imshow("red",img)
+        print("red")
+    elif a[int(a.shape[0])-int(a.shape[0]/6)][int(a.shape[1]/2)][1] ==255:
+        cv2.imshow("green",img)
+        print("green")
+    else:
+        cv2.imshow("yellow",img)
+        print("yellow")
+    cv2.waitKey()
