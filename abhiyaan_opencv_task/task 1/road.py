676a99277387953d75acb4980f760c2b64d4dc9b
ankitpal1029 <49815751+ankitpal1029@users.noreply.github.com>
2020-03-20 22:44:17 +0530

    Update road.py

--- abhiyaan_opencv_task/task 1/road.py
+++ abhiyaan_opencv_task/task 1/road.py
@@ -1,6 +1,6 @@
-import cv2
+import cv2  
 import numpy as np
-
+#source:open cv hough transform documentation
 img = cv2.imread('a.jpeg')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
